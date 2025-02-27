535278c4f56b6a1fa370ddf6c63e3aa6251d9a24
tbohn2 <tanner.bohn@yahoo.com>
2025-02-27 15:19:18 -0700

    Started basic layout; created two fetch functions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿async function getYear(year) {
+    const response = await fetch(`Year/${year}`,
+        {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+    if (!response.ok) {
+        console.error("Year not found");
+        return null;
+    }
+    return await response.json();
+}
 
-// Write your JavaScript code.
+async function addYear(yearValue) {
+    const newYear = {
+        YearValue: yearValue,
+    };
+
+    const response = await fetch("/AddYear", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newYear),
+    });
+
+    return await response.json();
+}
+
+$('#addYear').click(async function () {
+    const yearValue = $('#yearInput').val();
+    const year = await addYear(yearValue);
+    console.log(year);
+}
+);
+
+$(document).ready(async function () {
+    const yearVal = $('#year').text();
+    const year = await getYear(yearVal);
+    console.log(year);
+}
+);
