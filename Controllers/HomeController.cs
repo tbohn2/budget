da9ff8dc0174d9045fb5a30eacc64783138e585c
tbohn2 <tanner.bohn@yahoo.com>
2025-02-27 15:18:17 -0700

    Updated routes to get and create year

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 
 namespace Budget_App.Controllers
 {
-    [Route("[controller]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,6 +17,7 @@ namespace Budget_App.Controllers
             _context = context;
         }
 
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
@@ -43,19 +43,102 @@ namespace Budget_App.Controllers
         }
 
 
-        [HttpPost]
+        [HttpPost("AddYear")]
         public async Task<IActionResult> AddYear([FromBody] Year year)
         {
             if (ModelState.IsValid)
             {
+                Console.WriteLine(year.YearValue);
+
+                year.Months = new List<Month>();
+
+                for (int monthIndex = 1; monthIndex <= 12; monthIndex++)
+                {
+                    var month = new Month
+                    {
+                        Name = new DateTime(year.YearValue, monthIndex, 1).ToString("MMMM"),
+                        YearId = year.Id,
+                    };
+
+                    month.Expenses = new Expenses
+                    {
+                        MonthId = month.Id,
+                    };
+
+                    month.Earnings = new Earnings
+                    {
+                        MonthId = month.Id,
+                    };
+
+                    year.Months.Add(month);
+                }
+
                 _context.Years.Add(year);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+
+                // return RedirectToAction("Index");
             }
+
             return Json(year);
         }
 
-        [HttpDelete]
+        [HttpPut]
+        public async Task<IActionResult> UpdateExpenses([FromBody] Expenses expenses)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingExpense = await _context.Expenses.FindAsync(expenses.Id);
+
+                if (existingExpense == null)
+                {
+                    return NotFound();
+                }
+
+                existingExpense.Rent = expenses.Rent;
+                existingExpense.Tithing = expenses.Tithing;
+                existingExpense.Fast = expenses.Fast;
+                existingExpense.Groceries = expenses.Groceries;
+                existingExpense.Gas = expenses.Gas;
+                existingExpense.CarInsurance = expenses.CarInsurance;
+                existingExpense.Medical = expenses.Medical;
+                existingExpense.EatOut = expenses.EatOut;
+                existingExpense.Vacation = expenses.Vacation;
+                existingExpense.Holiday = expenses.Holiday;
+                existingExpense.Misc = expenses.Misc;
+
+                await _context.SaveChangesAsync();
+
+                return Json(existingExpense);
+            }
+
+            return Json(expenses);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEarnings([FromBody] Earnings earnings)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingEarnings = await _context.Earnings.FindAsync(earnings.Id);
+
+                if (existingEarnings == null)
+                {
+                    return NotFound();
+                }
+
+                existingEarnings.Primary = earnings.Primary;
+                existingEarnings.Secondary = earnings.Secondary;
+                existingEarnings.Gifts = earnings.Gifts;
+
+                await _context.SaveChangesAsync();
+
+                return Json(existingEarnings);
+            }
+
+            return Json(earnings);
+        }
+
+        [HttpDelete("DeleteYear/{id}")]
         public async Task<IActionResult> DeleteYear(int id)
         {
             var year = await _context.Years.FindAsync(id);
@@ -68,9 +151,12 @@ namespace Budget_App.Controllers
             return Json(year);
         }
 
-        public IActionResult Privacy()
+        [Route("{*url}")]
+        public IActionResult HandleUnknownRoute(string url)
         {
-            return View();
+            _logger.LogWarning($"Unknown route accessed: {url}");
+
+            return Json("Unknown route");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
