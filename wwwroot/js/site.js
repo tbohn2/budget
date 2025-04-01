09b9561bcd29001f979008e044d1f315d6366d30
tbohn2 <tanner.bohn@yahoo.com>
2025-04-01 16:19:10 -0700

    Created function to handle input change

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿let yearVal = new Date().getFullYear();
+let earningsToUpdate = {};
+let expensesToUpdate = {};
 
 async function getYear(year) {
     const response = await fetch(`Year/${year}`,
@@ -35,17 +37,32 @@ async function getTotal(object) {
         if (pair[0] === "id" || pair[0] === "monthId") { return; }
         total += pair[1];
     })
-    console.log(total);
 
     return total;
 }
 
-async function renderBudget(data) {
-    console.log(data);
+function handleChange(className, newValue, name, id) {
+    objectToUpdate = className === "earnings" ? earningsToUpdate : expensesToUpdate;
 
+    if (objectToUpdate[id] === undefined) {
+        const newObject = {};
+
+        $(`.${className}[data-id="${id}"]`).each(function () {
+            const element = $(this);
+            newObject[element.attr('name')] = parseInt(element.val());
+        })
+
+        newObject.id = id;
+        objectToUpdate[id] = newObject;
+    } else {
+        objectToUpdate[id][name] = newValue;
+    }
+}
+
+async function renderBudget(data) {
     const months = data.months;
 
-    months.forEach(async (month) => {
+    for (const month of months) {
         const id = month.name;
         const earnings = month.earnings;
         const expenses = month.expenses;
@@ -56,28 +73,38 @@ async function renderBudget(data) {
         const display = `
             <div class="values col-12">
                 <div class="grid-square space-holder"></div>
-                <input type="number" value="${earnings.primary}">
-                <input type="number" value="${earnings.secondary}">
-                <input type="number" value="${earnings.gifts}">
+                <input name="primary" data-id="${earnings.id}" class="earnings" type="number" value="${earnings.primary}">
+                <input name="secondary" data-id="${earnings.id}" class="earnings" type="number" value="${earnings.secondary}">
+                <input name="gifts" data-id="${earnings.id}" class="earnings" type="number" value="${earnings.gifts}">
                 <div class="grid-square space-holder">${earningsTotal}</div>
                 <div class="grid-square space-holder"></div>
-                <input type="number" value="${expenses.carInsurance}">
-                <input type="number" value="${expenses.eatOut}">
-                <input type="number" value="${expenses.fast}">
-                <input type="number" value="${expenses.gas}">
-                <input type="number" value="${expenses.groceries}">
-                <input type="number" value="${expenses.holiday}">
-                <input type="number" value="${expenses.medical}">
-                <input type="number" value="${expenses.misc}">
-                <input type="number" value="${expenses.rent}">
-                <input type="number" value="${expenses.tithing}">
-                <input type="number" value="${expenses.vacation}">
+                <input name="carInsurance" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.carInsurance}">
+                <input name="eatOut" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.eatOut}">
+                <input name="fast" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.fast}">
+                <input name="gas" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.gas}">
+                <input name="groceries" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.groceries}">
+                <input name="holiday" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.holiday}">
+                <input name="medical" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.medical}">
+                <input name="misc" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.misc}">
+                <input name="rent" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.rent}">
+                <input name="tithing" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.tithing}">
+                <input name="vacation" data-id="${expenses.id}" class="expenses" type="number" value="${expenses.vacation}">
                 <div class="grid-square space-holder">${expensesTotal}</div>
                 <div class="grid-square space-holder">${earningsTotal - expensesTotal}</div>
             </div>
             `
 
         $(`#${id}`).append(display)
+    };
+
+    $('body').on('change', '.earnings, .expenses', function () {
+        const input = $(this)
+        const className = input.attr('class')
+        const newValue = parseInt(input.val());
+        const name = input.attr('name');
+        const id = parseInt(input.attr('data-id'));
+
+        handleChange(className, newValue, name, id);
     });
 }
 
